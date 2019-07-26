5ddbfe69537d51bc2c331bdf4119cfd503d1acad
Mareike <mrkhartmann4@gmail.com>
2019-07-26 14:59:32 +0200

    write tmp outfile one json per line

--- preproc/preproc_mh17.py
+++ preproc/preproc_mh17.py
@@ -10,6 +10,7 @@ import itertools
 from deeppavlov import configs, build_model
 import logging
 from info_clusters.myutils import setup_logging, save_json
+import json
 
 
 # filter out urls
@@ -57,6 +58,7 @@ if __name__=="__main__":
     outfile = sys.argv[2]
     lang=sys.argv[3]
 
+    outfile_tmp = outfile + '.tmp'
 
     setup_logging('.', 'log.txt')
 
@@ -76,40 +78,44 @@ if __name__=="__main__":
 
     c = 0
     with codecs.open(infile, 'r', 'utf-8') as f:
-        for row in f:
-            tweet_lang = row.split('\t')[-2]
-            if tweet_lang == lang:
-                    tweet_data = {}
-                    tweet_id = row.split('\t')[0]
-                    tweet_text = row.split('\t')[9]
-                    # replace twitter lingo
-                    t = clean_text(repls, tweet_text)
-                    #lower case hashtags
-                    t = lowercase_hashtags(t)
-                    #tokenize
-                    doc = nlp(t)
-                    tweet = []
-                    for sentence in doc.sentences:
-                        s = ' '.join([word.text for word in sentence.words])
-                        tweet.append(s)
-                    tokenized_tweet = ' ##### '.join(tweet)
+        with codecs.open(outfile_tmp, 'a', 'utf-8') as fout:
+            for row in f:
+                tweet_lang = row.split('\t')[-2]
+                if tweet_lang == lang:
+                        tweet_data = {}
+                        tweet_id = row.split('\t')[0]
+                        tweet_text = row.split('\t')[9]
+                        # replace twitter lingo
+                        t = clean_text(repls, tweet_text)
+                        #lower case hashtags
+                        t = lowercase_hashtags(t)
+                        #tokenize
+                        doc = nlp(t)
+                        tweet = []
+                        for sentence in doc.sentences:
+                            s = ' '.join([word.text for word in sentence.words])
+                            tweet.append(s)
+                        tokenized_tweet = ' ##### '.join(tweet)
 
-                    # ner
-                    output = ner_model([tokenized_tweet])
+                        # ner
+                        output = ner_model([tokenized_tweet])
 
-                    # clean non alphanumerics
-                    cleaned_tokenized_tweet = ' '.join([tok.lower() for tok in tokenized_tweet.split(' ') if isalpha_or_hash(tok)])
+                        # clean non alphanumerics
+                        cleaned_tokenized_tweet = ' '.join([tok.lower() for tok in tokenized_tweet.split(' ') if isalpha_or_hash(tok)])
 
-                    tweet_data['tid'] = tweet_id
-                    tweet_data['text'] = tweet_text
-                    tweet_data['cleaned_text'] = cleaned_tokenized_tweet
-                    tweet_data['ner'] = {'text': output[0][0], 'tags': output[1][0]}
+                        tweet_data['tid'] = tweet_id
+                        tweet_data['text'] = tweet_text
+                        tweet_data['cleaned_text'] = cleaned_tokenized_tweet
+                        tweet_data['ner'] = {'text': output[0][0], 'tags': output[1][0]}
 
-                    data[tweet_id] = tweet_data
-                    c += 1
-                    if c % 100 == 0:
-                        logging.info('---> Processed {}'.format(c))
-                    if c > 1000:break
+                        data[tweet_id] = tweet_data
+                        c += 1
+                        if c % 100 == 0:
+                            logging.info('---> Processed {}'.format(c))
+                        if c > 1000:break
+
+                        fout.write(json.dumps(tweet_data))
+                        fout.write('\n')
 
     f.close()
     save_json(outfile, data)
